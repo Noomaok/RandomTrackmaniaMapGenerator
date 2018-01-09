8215270345d7ccebd7990f341ea7e2566a235ea4
Noomaok <noomaok@gmail.com>
2018-01-09 15:12:45 +0100

    Add files via upload

--- src/Blocks.java
+++ src/Blocks.java
@@ -1,21 +1,19 @@
 public class Blocks{
-    
-    private Vector3D relStartPoint;
+
     private Vector3D relEndPoint;
     private int blockID;
     private int startOrient;
     private int endOrient;
 
-    public Blocks(Vector3D rsPoint, Vector3D rePoint, int idBlock, int sOrient, int eOrient){
-        this.relStartPoint = rsPoint;
+    public Blocks(Vector3D rePoint, int idBlock, int sOrient, int eOrient){
         this.relEndPoint = rePoint;
         this.blockID = idBlock;
         this.startOrient = sOrient;
         this.endOrient = eOrient;
     }
 
-    public Vector3D getStartPoint(){
-        return relStartPoint;
+    public Blocks(double xEnd, double yEnd, double zEnd, int idBlock, int sOrient, int eOrient){
+        this(new Vector3D(xEnd, yEnd, zEnd), idBlock, sOrient, eOrient);
     }
 
     public Vector3D getEndPoint(){
--- src/Generator.java
+++ src/Generator.java
@@ -2,23 +2,32 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.JTextArea;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class Generator extends JFrame implements ActionListener{
     
+    private final int MAP_SIZE = 32;
+
     private Blocks[] blockList;
     private JButton generate;
     private JSlider lengthMap;
     private JLabel curLengthMap;
+    private JTextArea mapTextArea;
 
     public Generator(){
         //Create fenetre
@@ -61,25 +70,56 @@ public class Generator extends JFrame implements ActionListener{
         generate = new JButton("Generate Map");
         generate.addActionListener(this);
 
+        //Text where the map while be generate
+        mapTextArea = new JTextArea();
+        mapTextArea.setLineWrap(true);
+
         container.add(param);
         container.add(generate);
+        container.add(mapTextArea);
 
         this.getContentPane().add(container);
 
         //Show fen
         this.setVisible(true);
 
-
         //Load blocks
+        ArrayList<String> blockStringLoad = new ArrayList<String>();
+        File blockFile = new File("block.txt");
+        try {
+            Scanner sc = new Scanner(blockFile);
+            while(sc.hasNextLine()){
+                String s = sc.nextLine();
+                blockStringLoad.add(s);
+            }      
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-
+        for (int i = 0; i < blockStringLoad.size(); i++) {
+            String[] bInfo = blockStringLoad.get(i).split("/");
+            blockList[i] = new Blocks(Double.parseDouble(bInfo[0]), Double.parseDouble(bInfo[1]), Double.parseDouble(bInfo[2]), Integer.parseInt(bInfo[3]), Integer.parseInt(bInfo[4]), Integer.parseInt(bInfo[5]));
+        }
+        
         //Load presets
-
-
-
+        //Not needed actualy
     }
 
     public void actionPerformed(ActionEvent e){
+        generateMap();
+    }
 
+    public void generateMap(){
+        String mapStringExpression = "";
+
+        //Generate random start
+        int x = (int)(Math.random()*(MAP_SIZE-1))+1;
+        int y = (int)(Math.random()*(7))+9;
+        int z = (int)(Math.random()*(MAP_SIZE-1))+1;
+        int orient = (int)(Math.random()*(4));
+
+        mapStringExpression += x+"/"+y+"/"+z+"/"+9+"/"+orient;
+
+        mapTextArea.setText(mapStringExpression);
     }
 }
