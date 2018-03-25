4f8b966ab99f84bf5e66141ecf34a972af3e5d33
Noomaok <noomaok@gmail.com>
2018-03-25 22:18:10 +0200

    Block info modif

--- src/Block.java
+++ src/Block.java
@@ -1,32 +1,20 @@
 public class Block{
 
-    private Point3D extremPoint;
     private Point3D vectEntry;
     private Point3D vectExit;
     private boolean[][][] spaceOccupied;
-    private int blockId;
+    private String blockId;
 
-    public Block(Point3D exPoint, Point3D vEntry, Point3D vExit, String spaceString, int idBlock){
-        this.extremPoint = exPoint;
+    public Block(Point3D vEntry, Point3D vExit, boolean[][][] spOc, String idBlock){
         this.vectEntry = vEntry;
         this.vectExit = vExit;
         this.blockId = idBlock;
-        
-        spaceOccupied = new boolean[(int)(extremPoint.getX())][(int)(extremPoint.getY())][(int)(extremPoint.getZ())];
-        
-        int index = 0;
-        for(int i = 0; i<spaceOccupied.length; i++){
-            for(int j = 0; j<spaceOccupied[0].length; j++){
-                for(int k = 0; k<spaceOccupied[0][0].length; k++){
-                    spaceOccupied[i][j][k] = (spaceString.charAt(index) == '1');
-                    index++;
-                }
-            }
-        }
+        this.spaceOccupied = spOc;
     }
 
     public Point3D getEntry(){return vectEntry;}
     public Point3D getExit(){return vectExit;}
     public Point3D getExtrem(){return extremPoint;}
-    public int getId(){return blockId;}
+    public String getId(){return blockId;}
+    public boolean[][][] getSpace(){return spaceOccupied;}
 }
