2d75500c8c227057ac522509c0b0eadce56d8c99
Noomaok <noomaok@gmail.com>
2018-03-25 22:40:38 +0200

    add start block for test

--- src/BlockGene.java
+++ src/BlockGene.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+
 public class BlockGene{
-    
+    ArrayList<Block> elemBlocks = new ArrayList<Block>();
+
+    //Game block declaration
+    boolean[][][] st = {{{true}}};
+    Block start = new Block(null, 
+                            new Point3D(0, 0, 0), 
+                            st,  
+                            "StadiumRoadMainStartLine");
 }
